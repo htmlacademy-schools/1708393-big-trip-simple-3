fac7cea985ea8e59560964e7015b1b8548385441
Danil <chevychelov06122002@gmail.com>
2023-06-05 17:01:20 +0300

    m3t2_1

--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -1,10 +1,11 @@
 import CreationForm from '../view/creation-form';
-import EditForm from '../view/editing-form';
 import Sorting from '../view/sorting';
-import WaypointView from '../view/point';
-import WaypointList from '../view/point-list';
 import {render} from '../render';
 import {isEsc} from '../utils';
+import NoWaypointMessage from '../view/no-waypoints';
+import EditForm from '../view/edit-form';
+import WaypointView from '../view/waypoint';
+import WaypointList from '../view/waypoint-list';
 
 export default class BoardPresenter {
   #waypointListComponent = null;
@@ -19,13 +20,18 @@ export default class BoardPresenter {
 
   init() {
     const waypoints = [...this.#waypointsModel.arrWaypoints];
-    render(new Sorting(), this.#boardContainer);
-    this.#waypointListComponent = new WaypointList();
-    render(this.#waypointListComponent, this.#boardContainer);
-    render(new CreationForm(), this.#waypointListComponent.element);
-    this.#renderWaypoint(waypoints[0]);
-    for (let i = 1; i < 4; i++) {
-      this.#renderWaypoint(waypoints[i]);
+    if (waypoints.length === 0) {
+      this.#noWaypointMessage = new NoWaypointMessage();
+      render(this.#noWaypointMessage, this.#boardContainer);
+    } else {
+      render(new Sorting(), this.#boardContainer);
+      this.#waypointListComponent = new WaypointList();
+      render(this.#waypointListComponent, this.#boardContainer);
+      render(new CreationForm(), this.#waypointListComponent.element);
+      this.#renderWaypoint(waypoints[0]);
+      for (let i = 1; i < 4; i++) {
+        this.#renderWaypoint(waypoints[i]);
+      }
     }
   }
 
